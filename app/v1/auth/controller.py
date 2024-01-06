958d14bdaf539abe398e4aae6102180c96e69408
gunblasterXTO <naufal.abdila@outlook.com>
2024-01-06 23:17:55 +0700

    feat: slightly more proper user controller sample

--- app/v1/auth/controller.py
+++ app/v1/auth/controller.py
@@ -1,8 +1,25 @@
-# responsible for handling user input, updating the model,
-# intermediary between view (FE - interface) and the business logic
-from app.v1.auth.schema import LoginDTO
+# responsible for handling user input, business flow,
+# intermediary between view and the business logic (service).
+# stateless, handle particular request and not retain state between request.
+from app.v1.auth.dto import (
+    RegisterRequestDTO,
+    RegisterResponseDTO,
+    User
+)
 
 
-class AuthController:
-    def get_user(self, user: LoginDTO) -> bool:
-        return True
+class UserController:
+    def __init__(self):
+        pass
+
+    def get_user_by_id(self, user_id: int) -> User:
+        # in actual case might have database query here
+        return User(user_id=user_id, username="gunblasterXTO", pwd="")
+
+    def get_user_by_username(self, username: str) -> User:
+        return User(user_id=1, username=username, pwd="")
+
+    def create_new_user(
+        self, new_user: RegisterRequestDTO
+    ) -> RegisterResponseDTO:
+        return RegisterResponseDTO(user_id=2, username=new_user.username)
