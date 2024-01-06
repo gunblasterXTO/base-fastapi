39ddae067fbd8d46989562cb5344eec8063ec1e8
gunblasterXTO <naufal.abdila@outlook.com>
2024-01-06 23:17:02 +0700

    feat: dummy auth service

---
+++ app/v1/auth/service.py
@@ -0,0 +1,34 @@
+# responsible for cover business logic, core functionality of the application.
+# reusable, abstract from interface and focus on underlying business logic.
+# might be dependent to controller according to use cases.
+from fastapi import HTTPException, status
+
+from app.v1.auth.controller import UserController
+from app.v1.auth.dto import RegisterRequestDTO, User
+
+
+class AuthService:
+    def __init__(self, user_controller: UserController):
+        self.user_controller = user_controller
+
+    def register_new_user(self, new_user: RegisterRequestDTO) -> User:
+        existing_user = (
+            self.user_controller.get_user_by_username(new_user.username)
+        )
+        if existing_user:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="User is already registered"
+            )
+        user = self.user_controller.create_new_user(new_user)
+        return user
+
+    def authenticate_user(self, username: str, password: str) -> User:
+        user = self.user_controller.get_user_by_username(username)
+        if user and user.pwd == password:
+            return user
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid credentials"
+            )
