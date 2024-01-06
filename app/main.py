0a1cab49c8f668e280e1496639cd48ce7e4ae7de
gunblasterXTO <naufal.abdila@outlook.com>
2024-01-06 23:28:13 +0700

    feat: handle request body validation error custom response

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 from app.core.settings import Settings
+from app.helpers.response import BaseFailResponse
 from app.v1 import v1_router
 
 
@@ -9,3 +12,11 @@ app = FastAPI(
     version=Settings.VERSION
 )
 app.include_router(v1_router)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc) -> JSONResponse:
+    return JSONResponse(
+        content=BaseFailResponse(detail="Validation error").model_dump(),
+        status_code=status.HTTP_400_BAD_REQUEST
+    )
