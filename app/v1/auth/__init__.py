854cc4667976df40900c9635ca05e6c6717f1ce6
gunblasterXTO <naufal.abdila@outlook.com>
2024-01-06 23:17:27 +0700

    feat: add more view example

--- app/v1/auth/__init__.py
+++ app/v1/auth/__init__.py
@@ -5,4 +5,9 @@ from .view import AuthViews
 auth_views = AuthViews()
 auth_r = APIRouter(prefix="/auth")
 
-auth_r.add_api_route(path="/login", endpoint=auth_views.login, methods=["GET"])
+auth_r.add_api_route(
+    path="/login", endpoint=auth_views.login, methods=["GET"]
+)
+auth_r.add_api_route(
+    path="/register", endpoint=auth_views.registration, methods=["POST"]
+)
--- app/v1/auth/view.py
+++ app/v1/auth/view.py
@@ -3,15 +3,26 @@ from fastapi import status
 from fastapi.responses import JSONResponse
 
 from app.helpers.response import PostSuccessResponse
-from app.v1.auth.controller import AuthController
-from app.v1.auth.schema import LoginDTO
+from app.v1.auth.controller import UserController
+from app.v1.auth.dto import LoginRequestDTO, RegisterRequestDTO
+from app.v1.auth.service import AuthService
 
-auth_controller = AuthController()
+user_controller = UserController()
+auth_service = AuthService(user_controller)
 
 
 class AuthViews:
-    async def login(self, user: LoginDTO) -> JSONResponse:
-        _ = auth_controller.get_user(user)
+    async def registration(self, user: RegisterRequestDTO) -> JSONResponse:
+        new_user = auth_service.register_new_user(user).model_dump()
+        return JSONResponse(
+            content=PostSuccessResponse(data=new_user).model_dump(),
+            status_code=status.HTTP_201_CREATED
+        )
+
+    async def login(self, user: LoginRequestDTO) -> JSONResponse:
+        _ = auth_service.authenticate_user(
+            username=user.username, password=user.password
+        )
         return JSONResponse(
             content=PostSuccessResponse(data=[{}]).model_dump(),
             status_code=status.HTTP_200_OK
