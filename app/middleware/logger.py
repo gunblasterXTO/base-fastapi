a4d52166147e1e79b9f2b1926c377fd28dbddff6
gunblasterXTO <naufal.abdila@outlook.com>
2024-01-08 17:38:28 +0700

    fix: wrong indentation

--- app/middleware/logger.py
+++ app/middleware/logger.py
@@ -8,37 +8,37 @@ class LogMiddleware:
     def __init__(self, logger: CustomLogger) -> None:
         self.logger = logger
 
-        async def record_req(self, request: Request) -> None:
-            """
-            Record incoming request from client.
+    async def record_req(self, request: Request) -> None:
+        """
+        Record incoming request from client.
 
-            Args:
-                - request: request body from client-side
-            """
-            try:
-                payload = await request.json()
-            except Exception:
-                payload = None
+        Args:
+            - request: request body from client-side
+        """
+        try:
+            payload = await request.json()
+        except Exception:
+            payload = None
 
-            self.logger.accept(
-                url=request.url.path, method=request.method,
-                query_param=str(request.query_params), payload=payload
-            )
+        self.logger.accept(
+            url=request.url.path, method=request.method,
+            query_param=str(request.query_params), payload=payload
+        )
 
-        def record_resp(self, response: Response, time: float) -> None:
-            """
-            Record response to client.
+    def record_resp(self, response: Response, time: float) -> None:
+        """
+        Record response to client.
 
-            Args:
-                - response: response object that will be received by client
-                - time: time taken until process is completed
-            """
-            result = None
-            if 200 <= response.status_code < 300:
-                result = LogMsg.COMPLETE_REQ.value
-            elif 400 <= response.status_code < 500:
-                result = LogMsg.EXTERNAL_ERR_RESP.value
-            elif 500 >= response.status_code:
-                result = LogMsg.INTERNAL_ERR_RESP.value
+        Args:
+            - response: response object that will be received by client
+            - time: time taken until process is completed
+        """
+        result = ""
+        if 200 <= response.status_code < 300:
+            result = LogMsg.COMPLETE_REQ.value
+        elif 400 <= response.status_code < 500:
+            result = LogMsg.EXTERNAL_ERR_RESP.value
+        elif 500 >= response.status_code:
+            result = LogMsg.INTERNAL_ERR_RESP.value
 
-            self.logger.complete(result, time)
+        self.logger.complete(result, time)
