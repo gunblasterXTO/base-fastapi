28e2b405606d9f3f1d92a89febbf55bc81289abe
gunblasterXTO <naufal.abdila@outlook.com>
2024-01-14 19:58:52 +0700

    feat: add tags in router to better grouping the endpoint in the docs view

--- app/v1/auth/__init__.py
+++ app/v1/auth/__init__.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from .view import AuthViews
 
 auth_views = AuthViews()
-auth_r = APIRouter(prefix="/auth")
+auth_r = APIRouter(prefix="/auth", tags=["Auth"])
 
 auth_r.add_api_route(
     path="/login", endpoint=auth_views.login, methods=["GET"]
