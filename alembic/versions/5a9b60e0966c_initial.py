4050fd97ebc3785cb538c96e2622ec8a34fdafaf
gunblasterXTO <naufal.abdila@outlook.com>
2024-01-21 19:37:59 +0700

    feat: update alembic

--- alembic/versions/5a9b60e0966c_initial.py
+++ alembic/versions/5a9b60e0966c_initial.py
@@ -1,8 +1,8 @@
 """initial
 
-Revision ID: fef3cdd76a50
+Revision ID: 5a9b60e0966c
 Revises: 
-Create Date: 2024-01-21 19:12:03.011433
+Create Date: 2024-01-21 19:18:35.265370
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'fef3cdd76a50'
+revision: str = '5a9b60e0966c'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -23,23 +23,23 @@ def upgrade() -> None:
     op.create_table('roles',
     sa.Column('id', sa.SmallInteger(), nullable=False),
     sa.Column('name', sa.String(length=64), nullable=False),
+    sa.Column('is_active', sa.SmallInteger(), nullable=True),
     sa.Column('create_date', sa.DateTime(), nullable=False),
     sa.Column('update_date', sa.DateTime(), nullable=False),
-    sa.Column('is_active', sa.SmallInteger(), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_roles_id'), 'roles', ['id'], unique=False)
     op.create_table('users',
+    sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('id_hash', sa.String(length=256), nullable=False),
     sa.Column('name', sa.String(length=256), nullable=False),
     sa.Column('email', sa.String(length=128), nullable=False),
     sa.Column('pass_hash', sa.String(length=123), nullable=False),
     sa.Column('pass_salt', sa.String(length=128), nullable=False),
     sa.Column('id_role', sa.SmallInteger(), nullable=True),
-    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('is_active', sa.SmallInteger(), nullable=True),
     sa.Column('create_date', sa.DateTime(), nullable=False),
     sa.Column('update_date', sa.DateTime(), nullable=False),
-    sa.Column('is_active', sa.SmallInteger(), nullable=True),
     sa.ForeignKeyConstraint(['id_role'], ['roles.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
