7804df07f75df550de03121cfbbb77f196662da5
gunblasterXTO <naufal.abdila@outlook.com>
2024-01-21 19:34:50 +0700

    id move to each model class

--- app/db/models/base.py
+++ app/db/models/base.py
@@ -1,18 +1,17 @@
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, Integer, SmallInteger
+from sqlalchemy import Column, DateTime, SmallInteger
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import as_declarative
 
 
 @as_declarative()
 class Base:
-    id = Column(Integer, primary_key=True, index=True)
+    is_active = Column(SmallInteger, default=1)  # 1: active, 0: inactive
     create_date = Column(DateTime, default=datetime.now, nullable=False)
     update_date = Column(
         DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
     )
-    is_active = Column(SmallInteger, default=1)  # 1: active, 0: inactive
 
     # to generate tablename from classname
     @declared_attr
--- app/db/models/user_mgmt.py
+++ app/db/models/user_mgmt.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, ForeignKey, SmallInteger, String
+from sqlalchemy import Column, ForeignKey, Integer, SmallInteger, String
 
 from app.db.models.base import Base
 from app.db.models.util import ModelsUtil
 
 
 class Users(Base):
+    id = Column(Integer, primary_key=True, index=True)
     id_hash = Column(
         String(256), nullable=False, default=ModelsUtil.generate_hash
     )
